11c78570a20464cd37b03f0ec7a0ceaf6435e630
Akash <akash.thakare@thoughtworks.com>
2024-05-24 19:54:19 +0530

    Update python script. Clean up.

---
+++ _blogops.py
@@ -0,0 +1,97 @@
+import json
+import os
+import sys
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+from google.oauth2.credentials import Credentials
+from googleapiclient.errors import HttpError
+from bs4 import BeautifulSoup
+
+SCOPES = ['https://www.googleapis.com/auth/blogger']
+REDIRECT_URI = os.environ['REDIRECT_URI']
+CLIENT_SECRETS_FILE = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+TOKEN_FILE = os.environ['TOKEN_FILE']
+BLOG_ID = os.environ['BLOG_ID']
+
+def refresh_token():
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
+    flow.redirect_uri = REDIRECT_URI
+    credentials = flow.run_local_server(port=8080)
+    
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
+
+def init():
+    service = _init_service_()
+    title = sys.argv[2]
+    
+    post_body = {
+        "kind": "blogger#post",
+        "title": f'"{title}"'
+    }
+
+    try:
+        posts = service.posts()
+        request = posts.insert(blogId=BLOG_ID, isDraft=True, body=post_body)
+        response = request.execute()
+        postId = response['id']
+        
+        folderPath = f'./blogposts/{title}'
+        os.mkdir(folderPath)
+        os.mkdir(f'{folderPath}/images')
+        print("Inside init2")
+        mdfile = open(f"{folderPath}/{title}.md", "w")
+        mdfile.write(f"""---
+layout: post
+permalink: /
+title: {title}
+post: {postId}
+---
+""")
+        
+        print(f"Draft created with ID: {postId}")
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
+def update():
+    post = _post_()
+    service = _init_service_()
+
+    try:
+        posts = service.posts()
+        request = posts.update(blogId=BLOG_ID, postId=post[0], body=post[1])
+        response = request.execute()
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
+def _init_service_():
+    credentials = Credentials.from_authorized_user_file(TOKEN_FILE, SCOPES)
+    service = googleapiclient.discovery.build('blogger', 'v3', credentials=credentials)
+    return service
+
+def _post_():
+    with open(sys.argv[2], 'r') as file:
+        soup = BeautifulSoup(file.read(), "html.parser")
+            
+        for img in soup.findAll('img'):
+            img['src'] = 'https://github.com/aakashthakare/blog/blob/main/blogposts/' + sys.argv[2] + '/' + img['src'] + '?raw=true'
+        
+        postid = soup.find(id = 'atptid').string
+        title = soup.find(id = 'titleid').string
+
+        soup.find('span', id="atptid").decompose()
+        soup.find('span', id="titleid").decompose()
+
+        body = str(soup.find('div', {"id": "post-main"}))
+
+        post_html = {
+            "kind": "blogger#post",
+            "title": f"{title}",
+            "content": f"{body}"
+        }
+
+        return postid, post_html
+
+if __name__ == '__main__':
+    globals()[sys.argv[1]]()
--- blog.sh
+++ blog.sh
@@ -11,15 +11,17 @@ IS_INIT=$([ -d "$DIR" ] && echo 1 || echo 0)
 if [ $IS_INIT == 0 ];
 then
    echo "Initializing $blogpost..."
-   python3 _init.py "$blogpost"
+   python3 _blogops.py init "$blogpost"
 else
     echo "Updating $blogpost..."
     scp -r "blogposts/$blogpost" "jekyll/"
     jekyll build -s "jekyll/" -d "output/$blogpost"
-    mv output/$blogpost/$blogpost/* output/$blogpost/
+    if [ -f output/$blogpost/$blogpost/* ]; then
+        mv output/$blogpost/$blogpost/* output/$blogpost/
+    fi
     rm -rf "jekyll/$blogpost"
     rm -rf output/$blogpost/$blogpost
     indexHtml=;
     echo $indexHtml
-    python3 "_update.py" "$(pwd)/output/$blogpost/index.html" "$blogpost"
+    python3 _blogops.py update "$(pwd)/output/$blogpost/index.html" "$blogpost"
 fi
